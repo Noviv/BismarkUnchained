e01a48056e6ff13350ed60c0da079bcca42c99b7
Matthew Webb <mwebbmwebb@gmail.com>
2019-11-14 17:49:40 -0600

    switch from get_node to $

--- BismarkUnchained/scenes/Bullet/Bullet.gd
+++ BismarkUnchained/scenes/Bullet/Bullet.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 func explode():
-	get_node('Explosion').global_position = get_node('Body').global_position
-	get_node('Explosion').emitting = true
-	get_node('FreeTimer').connect('timeout', self, 'queue_free')
-	get_node('FreeTimer').start()
+	$Explosion.global_position = get_node('Body').global_position
+	$Explosion.emitting = true
+	$FreeTimer.connect('timeout', self, 'queue_free')
+	$FreeTimer.start()
 
 # vel is a Vector2
 func init(vel):
-	get_node('Body').velocity = vel
+	$Body.velocity = vel
