001b0176eab9e5c222d348ca431d82949d86f4dc
vmorandi <35947566+vmorandi@users.noreply.github.com>
2019-11-14 20:04:25 -0600

    add enemy following/collision (#7)
    Closes #2

---
+++ BismarkUnchained/scenes/Enemy/Enemy.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+signal enemy_hit
+
+
+func _ready():
+ 	pass
+
+func _process(delta):
+	var pos = get_global_position()
+	var player_pos = get_node("../../Player/KinematicBody2D").get_global_position()
+	var dir = (player_pos - pos).normalized()
+	var collision = move_and_collide(dir * get_node("/root/Main").time_delta)
+	if collision:
+		print("hit")
+		dir = dir.bounce(collision.normal)
