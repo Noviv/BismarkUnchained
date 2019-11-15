0f3a9ac5bbeacff28ebe2dbdf2ed3a46c894f4b9
Shray Mittal <shray@shraymittal.dev>
2019-11-15 16:20:43 -0600

    simultaneous keyboard and controller support

--- BismarkUnchained/scenes/KinematicBody2D.gd
+++ BismarkUnchained/scenes/KinematicBody2D.gd
@@ -9,22 +9,23 @@ func damage():
 	print('hit player')
 
 func _process(delta):
-	#var velocity = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
-	#if (velocity.length() < 0.2):
-	#	velocity = Vector2(0, 0)
-	#if (velocity.length() > 1):
-	#	velocity = velocity.normalized()
-	#velocity = velocity * velocity * velocity
+	var velocity = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
+	if (velocity.length() < 0.2):
+		velocity = Vector2(0, 0)
+	if (velocity.length() > 1):
+		velocity = velocity.normalized()
+	velocity = velocity * velocity * velocity
 	
-	var velocity = Vector2(0, 0)
-	if Input.is_action_pressed("player_up"):
-		velocity += Vector2(0, -1)
-	if Input.is_action_pressed("player_down"):
-		velocity += Vector2(0, 1)
-	if Input.is_action_pressed("player_left"):
-		velocity += Vector2(-1, 0)
-	if Input.is_action_pressed("player_right"):
-		velocity += Vector2(1, 0)
+	if (velocity.length() == 0):
+		velocity = Vector2(0, 0)
+		if Input.is_action_pressed("player_up"):
+			velocity += Vector2(0, -1)
+		if Input.is_action_pressed("player_down"):
+			velocity += Vector2(0, 1)
+		if Input.is_action_pressed("player_left"):
+			velocity += Vector2(-1, 0)
+		if Input.is_action_pressed("player_right"):
+			velocity += Vector2(1, 0)
 	
 	velocity = velocity * max_velocity
 	var diff_velocity = velocity - curr_velocity
