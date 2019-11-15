523eebe601bf32df0e91d89245f9e0df35e0d603
Matthew Webb <mwebbmwebb@gmail.com>
2019-11-15 15:38:14 -0600

    Switch from bullet damage signal to function call (#15)

--- BismarkUnchained/scenes/Bullet/Body.gd
+++ BismarkUnchained/scenes/Bullet/Body.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-signal collided
-
 var velocity = Vector2(100, 0)
 
 func _physics_process(delta):
@@ -11,6 +9,7 @@ func _physics_process(delta):
 	# move and check for a collision
 	var kc = move_and_collide(velocity * delta)
 	if kc:
-		emit_signal('collided', kc.collider)
+		if kc.collider.has_method('damage'):
+			kc.collider.damage()
 		get_parent().explode()
 		queue_free()
