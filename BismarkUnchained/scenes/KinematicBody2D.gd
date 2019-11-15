ae932cf419f5f35e22f4c55de49e4d486c76fd9c
Shray Mittal <shray@shraymittal.dev>
2019-11-15 16:11:09 -0600

    added controller movement commented. player speed scales time delta

--- BismarkUnchained/scenes/KinematicBody2D.gd
+++ BismarkUnchained/scenes/KinematicBody2D.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+var accel = 50
+var max_velocity = 10
+
 var velocity
 var curr_velocity = Vector2(0, 0)
 
@@ -7,8 +10,14 @@ func damage():
 	print('hit player')
 
 func _process(delta):
+	#velocity = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
+	#if (velocity.length() < 0.2):
+	#	velocity = Vector2(0, 0)
+	#if (velocity.length() > 1):
+	#	velocity = velocity.normalized()
+	#velocity = velocity * velocity * velocity
+	
 	velocity = Vector2(0, 0)
-	#This will change to controller input
 	if Input.is_action_pressed("player_up"):
 		velocity += Vector2(0, -1)
 	if Input.is_action_pressed("player_down"):
@@ -17,16 +26,16 @@ func _process(delta):
 		velocity += Vector2(-1, 0)
 	if Input.is_action_pressed("player_right"):
 		velocity += Vector2(1, 0)
-	velocity = velocity.normalized() * 10
+	
+	velocity = velocity * max_velocity
 	var diff_velocity = velocity - curr_velocity
-	var accel = 30
-	if diff_velocity.length() >= 10 || velocity.length() == 0:
-		accel = 60
+	
 	var new_velocity = curr_velocity + diff_velocity.normalized() * accel * delta
-	if new_velocity.length() > 10:
+	if new_velocity.length() > velocity.length():
 		new_velocity = velocity
 	if new_velocity.length() < 0.5 && velocity.length() == 0:
 		new_velocity = velocity
 	velocity = new_velocity
 	move_and_collide(velocity)
 	curr_velocity = velocity
+	get_node("/root/Main").time_delta = curr_velocity.length() / 15
