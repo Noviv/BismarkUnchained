8c4641946367c9456b1072fec12474c9763c4b21
Ben Hazel <benhazel@cs.utexas.edu>
2019-11-15 15:42:46 -0600

    random spawning

--- BismarkUnchained/scenes/WaveHandler/WaveHandler.gd
+++ BismarkUnchained/scenes/WaveHandler/WaveHandler.gd
@@ -2,16 +2,21 @@ extends Node2D
 
 var wave_num = 0
 var wave_reminder_sent = false
+var wave_length = 1
+var wave_warning = .5
 onready var TempEnemy = load("res://scenes/WaveHandler/TempEnemy.tscn")
+onready var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Wave ready")
 	start_next_wave()
+	var time = OS.get_time()
+	rng.set_seed((time.hour + time.minute + time.second))
 	
 func _process(delta):
-	if($WaveTimer.get_time_left() < 5 && !wave_reminder_sent):
-		print("Wave " + String(wave_num + 1) + " starting in 5 seconds")
+	if($WaveTimer.get_time_left() < wave_warning && !wave_reminder_sent):
+		print("Wave " + String(wave_num + 1) + " starting in " + String(wave_warning) +" seconds")
 		wave_reminder_sent = true
 
 func start_next_wave():
@@ -19,10 +24,10 @@ func start_next_wave():
 	print("Wave " + String(wave_num) + " starting now")
 	
 	var tempSpawn = TempEnemy.instance()
-	tempSpawn.position = Vector2(wave_num * 40, wave_num * 40)
+	tempSpawn.position = Vector2(rng.randi_range(50, 1150), rng.randi_range(50, 650))
 	get_parent().call_deferred("add_child", tempSpawn)
 	
-	$WaveTimer.start(10.0)
+	$WaveTimer.start(float(wave_length))
 	wave_reminder_sent = false
 
 func _on_Timer_timeout():
