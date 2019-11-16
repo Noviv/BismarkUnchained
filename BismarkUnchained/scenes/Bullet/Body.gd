d20361d96322c7fe0d4ccc24482a1f7a0da79184
Matthew Webb <mwebbmwebb@gmail.com>
2019-11-16 16:36:23 -0600

    Support bullet and explosion time-delta-scaling

--- BismarkUnchained/scenes/Bullet/Body.gd
+++ BismarkUnchained/scenes/Bullet/Body.gd
@@ -7,9 +7,9 @@ func _physics_process(delta):
 	rotation = velocity.angle()
 	
 	# move and check for a collision
-	var kc = move_and_collide(velocity * delta)
+	var kc = move_and_collide(velocity * get_node("/root/Main").time_delta * delta)
 	if kc:
 		if kc.collider.has_method('damage'):
 			kc.collider.damage()
-		get_parent().explode()
+		get_parent().explode(kc.position)
 		queue_free()
--- BismarkUnchained/scenes/Bullet/Bullet.gd
+++ BismarkUnchained/scenes/Bullet/Bullet.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
-func explode():
-	$Explosion.global_position = get_node('Body').global_position
+var time_left = 0
+
+func explode(pos):
+	$Explosion.global_position = pos
 	$Explosion.emitting = true
-	$FreeTimer.connect('timeout', self, 'queue_free')
-	$FreeTimer.start()
+	
+	# allow time for all particles to expire before destroying
+	time_left = $Explosion.lifetime * 1.5
+
+func _process(delta):
+	if $Explosion.emitting and time_left < 0:
+		queue_free()
+	$Explosion.speed_scale = get_node("/root/Main").time_delta
+	time_left -= delta * get_node("/root/Main").time_delta
 
 # vel is a Vector2
 func init(vel):
