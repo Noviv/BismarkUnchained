975c5bb938689edad43ae3393b98c24d3e2e64c0
thicctor <vmorandi@utexas.edu>
2019-11-17 18:28:30 -0600

    added enemy rotation and fixed bullet spawn time

--- BismarkUnchained/scenes/Enemy/Enemy.gd
+++ BismarkUnchained/scenes/Enemy/Enemy.gd
@@ -1,40 +1,37 @@
 extends KinematicBody2D
 
 onready var sprite = preload("res://scenes/Bullet/Bullet.tscn")
-var timer = null
-var timer_delay = .5
-var can_shoot = true
+var time_left = 0
 var health
 
 func _ready():
-	timer= Timer.new()
-	timer.set_one_shot(true)
-	timer.set_wait_time(timer_delay)
-	timer.connect("timeout", self, "on_timeout_complete")
-	add_child(timer)
 	health = 100
 
-func on_timeout_complete():
-       can_shoot = true
-
 func damage():
-       health -= 20
-
+	health -= 20
 
 func _process(delta):
+	var time_scale = get_node("/root/Main").get_time_delta()
 	if(health <= 0):
 		queue_free()
 		pass
 	var pos = get_global_position()
 	var player_pos = get_node("../../Player/PlayerBody").get_global_position()
 	var dir = (player_pos - pos).normalized()
-	var collision = move_and_collide(dir * get_node("/root/Main").get_time_delta())
+	var collision = move_and_collide(dir * time_scale)
+	var aim_speed = deg2rad(1)*3.2
+	if get_angle_to(player_pos) > 0:
+    	rotation += aim_speed * time_scale
+	elif get_angle_to(player_pos) < 0:
+    	rotation -= aim_speed * time_scale
 	if collision:
 		print("hit")
 		dir = dir.bounce(collision.normal)
-	if pos.y > player_pos.y - 50 && pos.y < player_pos.y + 50 && can_shoot:
+	if get_angle_to(player_pos) < 0.5 && get_angle_to(player_pos) > -0.5 && time_left < 0:
 		var s = sprite.instance()
-		s.position += Vector2(30, 0)
-		add_child(s)
-		can_shoot = false
-		timer.start()  
+		s.position += Vector2(player_pos.x - pos.x, player_pos.y - pos.y)
+		s.get_node("Body").velocity = Vector2(player_pos.x - pos.x, player_pos.y - pos.y).normalized() * 100
+		get_parent().add_child(s)
+		time_left = .5
+	else:
+		time_left -= delta * time_scale
