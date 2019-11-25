b67c3b766f052872521c5b3ba5569853b1293f14
Shray Mittal <shray@shraymittal.dev>
2019-11-24 19:41:52 -0600

    Added recharge progressbar. Added new bullet color + size + fastter speed for player shooting

--- BismarkUnchained/scenes/Player/Player.gd
+++ BismarkUnchained/scenes/Player/Player.gd
@@ -10,6 +10,7 @@ var device = -1
 
 var time_last_shot = -1
 var time_to_shoot = 0.75
+var bullet_velocity = 1000
 onready var bullet_sprite = preload("res://scenes/Bullet/Bullet.tscn")
 var last_dir = Vector2(0, 0)
 
@@ -80,13 +81,17 @@ func shoot():
 		if get_node("/root/Main").time_elapsed > time_last_shot + time_to_shoot:
 			time_last_shot = get_node("/root/Main").time_elapsed
 			var sprite = bullet_sprite.instance()
+			sprite.set_scale(Vector2(1.4, 1.4))
+			sprite.set_modulate(Color(0, 0, 0, 1))
 			var x_vel = 0
 			var y_vel = 0
 			x_vel = cos(atan2(last_dir.y, last_dir.x))
 			y_vel = sin(atan2(last_dir.y, last_dir.x))
-			sprite.get_node("Body").velocity = Vector2(x_vel, y_vel).normalized() * 100
+			sprite.get_node("Body").velocity = Vector2(x_vel, y_vel).normalized() * bullet_velocity
 			get_parent().add_child(sprite)
-			sprite.global_position = get_global_position() + sprite.get_node("Body").velocity.normalized() * 30
+			sprite.global_position = get_global_position() + sprite.get_node("Body").velocity.normalized() * 50
+			get_node("/root/Main/UI/WeaponRecharge").value = 0
+	get_node("/root/Main/UI/WeaponRecharge").value = 100 * (get_node("/root/Main").time_elapsed - time_last_shot) / time_to_shoot
 			
 
 func rotate_dir(direction):
