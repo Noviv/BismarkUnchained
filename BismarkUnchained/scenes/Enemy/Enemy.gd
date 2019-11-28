636161684bc51f88d309d257ffb2c2f8be987eab
Matthew Webb <mwebbmwebb@gmail.com>
2019-11-27 22:52:11 -0600

    More formatting

--- BismarkUnchained/scenes/Enemy/Enemy.gd
+++ BismarkUnchained/scenes/Enemy/Enemy.gd
@@ -17,20 +17,22 @@ func _process(delta):
 	if health <= 0:
 		get_node("/root/Main").up_score(100)
 		queue_free()
-
+	
 	var pos = get_global_position()
 	var player_pos = get_node("../../Player/PlayerBody").get_global_position()
 	var dir = (player_pos - pos).normalized()
 	var collision = move_and_collide(dir * time_scale)
+	
 	if get_angle_to(player_pos) > 0:
 		rotation += aim_speed * time_scale
 	elif get_angle_to(player_pos) < 0:
 		rotation -= aim_speed * time_scale
+	
 	if get_angle_to(player_pos) < 0.5 && get_angle_to(player_pos) > -0.5 && time_left < 0 && player_pos.distance_to(pos) > 150:
 		var s = sprite.instance()
 		s.get_node("Body").velocity = Vector2(player_pos.x - pos.x, player_pos.y - pos.y).normalized() * 100
 		get_parent().add_child(s)
-		s.global_position = Vector2(pos.x - .4*(pos.x - player_pos.x)/2, pos.y - .4*(pos.y - player_pos.y)/2)
+		s.global_position = Vector2(pos.x - .4 * (pos.x - player_pos.x) / 2, pos.y - .4 * (pos.y - player_pos.y) / 2)
 		time_left = .5
 	else:
 		time_left -= delta * time_scale
--- BismarkUnchained/scenes/Main/Main.gd
+++ BismarkUnchained/scenes/Main/Main.gd
@@ -7,26 +7,26 @@ var time_elapsed = 0
 var min_time_delta = 0.1
 onready var WUI = get_node("/root/Main//UI")
 
-
 func get_time_delta():
 	return time_delta
 
 func set_time_delta(new_delta):
-	#Need a buffer on new_delta to account for floating point precision errors
+	# Need a buffer on new_delta to account for floating point precision errors
 	if new_delta < -0.001 || new_delta > 1.001:
 		print("time_delta out of bounds")
 		print(new_delta)
 		get_tree().quit()
+	
 	time_delta = new_delta * (1 - min_time_delta) + min_time_delta
 	if time_delta < min_time_delta:
 		time_delta = min_time_delta
-	if time_delta > 1:
+	elif time_delta > 1:
 		time_delta = 1
 	get_node("/root/Main/UI/TimeDelta").value = time_delta * 100
-	
+
 func up_score(points):
 	score += points
 	WUI.get_node("Score").set_text(String(score))
-	
+
 func _process(delta):
 	time_elapsed += delta * time_delta
