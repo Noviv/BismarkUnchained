fe7c083aea5e4f7d01ce0c78764b848dc0e7a85e
Ben Hazel <benhazel@cs.utexas.edu>
2019-12-01 18:50:44 -0600

    starts at wave 1 again, oops

--- BismarkUnchained/scenes/WaveHandler/WaveHandler.gd
+++ BismarkUnchained/scenes/WaveHandler/WaveHandler.gd
@@ -8,7 +8,7 @@ onready var rng = RandomNumberGenerator.new()
 const wave_length = 12
 const wave_warning = 6
 
-var wave_num = 10
+var wave_num = 1
 var wave_reminder_sent = false
 
 # Called when the node enters the scene tree for the first time.
